3d68144d53a4a10a9883359c89053eca2e54d564
Sanskar Chandaliya <73470491+Suns-Caar@users.noreply.github.com>
2023-10-27 23:35:07 +0530

    Add files via upload

---
+++ Minized_no_accel/Debug/src/helloworld.d
@@ -0,0 +1,64 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xcanps.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xcanps_hw.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xsdps.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xsdps_hw.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xuartps_hw.h
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xcanps.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xcanps_hw.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xsdps.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xsdps_hw.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
---
+++ Minized_no_accel/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../Minized_no_accel_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
